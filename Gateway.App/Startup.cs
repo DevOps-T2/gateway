02cbd0e7ac6ec2fa49d796d1f475a66e97717161
Andreas Olesen <dresing@live.dk>
2021-12-13 12:23:53 +0100

    Update readme

---
+++ Gateway.App/Startup.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.OpenApi.Models;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace Gateway.App
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+ 
+            services.AddRazorPages();
+            services.AddControllersWithViews();
+
+
+            services.AddSwaggerForOcelot(Configuration);
+
+            services.AddEndpointsApiExplorer();
+
+            services.AddOcelot(Configuration);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+           
+            // Configure the HTTP request pipeline.
+            if (!env.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            /*var subscriber = new AutoSubscriber(app.ApplicationServices.GetRequiredService<IBus>(), "solvers")
+            {
+                AutoSubscriberMessageDispatcher = new ScopeDispatcher(app.ApplicationServices.GetRequiredService<IServiceScopeFactory>())
+            };
+
+            subscriber.Subscribe(Assembly.GetExecutingAssembly().GetTypes());
+            subscriber.SubscribeAsync(Assembly.GetExecutingAssembly().GetTypes()); */
+
+            app.UseSwaggerForOcelotUI(opt =>
+            {
+                opt.PathToSwaggerGenerator = "/swagger/docs";
+            });
+
+
+            app.UseOcelot();
+
+
+
+
+        }
+    }
+
+}
